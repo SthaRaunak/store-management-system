b104ed16d344a439c5919d2ee1c87dd7a03e19e4
SthaRaunak <codewithraunak2006@gmail.com>
2024-02-17 22:44:43 +0545

    config prettier and db connection

---
+++ server/db/connection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.MONGODB_DATABASE}`);
+        console.log(`MongoDB connected successfullly! DB HOST: ${connectionInstance.connection.host}`);
+  } catch (error) {
+        console.log("MongDB connection failed! ",error);
+        process.exit(1);     //ends the node process
+};
+};
+
+export default connectDB;
--- server/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '../client/node_modules/vite/dist/node'
-import react from '../client/node_modules/@vitejs/plugin-react/dist'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
